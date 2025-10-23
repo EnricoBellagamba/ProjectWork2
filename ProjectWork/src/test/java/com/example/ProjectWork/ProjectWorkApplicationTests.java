a4662ef14d802756e3a3ab4aa249b348fa03a7e4
Enrico <enri.bellagamba@gmail.com>
2025-10-23 12:05:19 +0200

    primo commit spring boot

---
+++ ProjectWork/src/test/java/com/example/ProjectWork/ProjectWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
