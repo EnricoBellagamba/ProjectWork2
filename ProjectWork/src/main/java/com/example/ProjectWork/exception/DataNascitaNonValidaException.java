1bf5dfe3f87cee7b1c0d37e7cb7049b962c24991
Aria Tafazoli <159703070+AriaDev98@users.noreply.github.com>
2025-11-19 22:55:11 +0100

    1)aggiornato AuthService per gestire il cv , salvarlo sul locale(FileSystem) e dopodiche settarlo come cvUrl nel db.   2) al momento login di un utente , si aggiorna LastLogin.

---
+++ ProjectWork/src/main/java/com/example/ProjectWork/exception/DataNascitaNonValidaException.java
@@ -0,0 +1,8 @@
+package com.example.ProjectWork.exception;
+
+public class DataNascitaNonValidaException extends RuntimeException  {
+
+    public DataNascitaNonValidaException(String message) {
+        super(message);
+    }
+}
