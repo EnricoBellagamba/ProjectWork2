f420d204293e823c5cb8ee34fecb175f0ff14426
RiccardoFermi <riccardofermi9@gmail.com>
2025-11-24 14:26:45 +0100

    Posizioni candidati completa

--- ProjectWork/src/main/java/com/example/ProjectWork/repository/PosizioneRepository.java
+++ ProjectWork/src/main/java/com/example/ProjectWork/repository/PosizioneRepository.java
@@ -3,20 +3,15 @@ package com.example.ProjectWork.repository;
 import com.example.ProjectWork.model.Posizione;
 import com.example.ProjectWork.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PosizioneRepository extends JpaRepository<Posizione, Long> {
 
-    @Query(value = """
-            SELECT TOP 4 *
-            FROM dbo.POSIZIONE
-            ORDER BY pubblicataAt DESC;
-        """, nativeQuery = true)
-    public List<Posizione> topQuattroPosizioni();
-
-    //tutte le posizioni create da un certo HR
-    List<Posizione> findByCreatedByHR(Utente hr);
+    /**
+     * Ritorna tutte le posizioni create da un certo HR.
+     * Usato dall'endpoint GET /api/posizioni/hr/mie
+     */
+    List<Posizione> findByCreatedByHR(Utente createdByHR);
 
 }
